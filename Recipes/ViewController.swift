17289f5df12accd52ed28a729d373f9dd66a5e4f
Dmytriv Oleh <31968736+OlehDmytriv@users.noreply.github.com>
2019-08-04 23:46:43 -0400

    Initial Commit

---
+++ Recipes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Recipes
+//
+//  Created by Oleh Dmytriv on 2019-08-04.
+//  Copyright © 2019 Oleh Dmytriv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
